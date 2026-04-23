00ebdba3c5ec7ebd40464e0d96936bb0380c05f1
Sayd Barrera <barrerasayd7@gmail.com>
2026-04-22 19:30:06 -0500

    se añadió los import en user.routers.js

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,19 @@
 import { Router } from "express";
 import { register, login, getUsuarios, getUsuarioById, updateUsuario, verifyToken } from "../controllers/user.controller.js";
+import { seguirUsuario, dejarDeSeguir, estadoSeguimiento } from "../controllers/seguidor.controller.js";
 const router = Router();
 
 router.get("/", verifyToken, getUsuarios);
+router.get("/seguimiento",     estadoSeguimiento);
 router.get("/:id", verifyToken, getUsuarioById);
 router.post("/register", register);
 router.post("/login", login);
 router.put("/:id", verifyToken, updateUsuario);
+
+
+// ── Seguimientos ──
+router.post("/seguir",         seguirUsuario);
+router.delete("/dejar-seguir", dejarDeSeguir);
+
 
 export default router;
